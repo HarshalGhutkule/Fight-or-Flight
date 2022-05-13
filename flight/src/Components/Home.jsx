544c52063c8bd56150adbed55e823f4142318121
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2022-05-14 01:06:13 +0530

    flight table added

--- flight/src/Components/Home.jsx
+++ flight/src/Components/Home.jsx
@@ -1,13 +1,15 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import styled from "styled-components";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Grid from '@mui/material/Grid';
+import Tooltip from '@mui/material/Tooltip';
+import LinearProgress from '@mui/material/LinearProgress';
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 export const Home = () => {
+  const [flight,setFlight] = useState([]);
+
   const Main = styled.div`
     display: flex;
     justify-content: space-evenly;
@@ -32,6 +34,26 @@ export const Home = () => {
     }
   `;
 
+  const SubMain = styled.div`
+    background-color: white;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    & > div>div{
+      width:15%;
+    }
+  `;
+
+  useEffect(()=>{
+    //getFlight();
+  },[])
+
+  const getFlight = ()=>{
+    axios.get('https://app.goflightlabs.com/flights?access_key=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiMDE1MTNhNjczNzVhYjIyNDU4YzMxZjkyMjM2Mjc5MzQ0MjNhN2Y5YTZmNTFmODY3MGJlNTAwOTg0NGUzMjBiZjI1YmRlNjE4YmEzMmY4ZDYiLCJpYXQiOjE2NTIyMTA0MzUsIm5iZiI6MTY1MjIxMDQzNSwiZXhwIjoxNjgzNzQ2NDM0LCJzdWIiOiI0Mjk0Iiwic2NvcGVzIjpbXX0.mMHLpEmemmAlUB9mavYajijxepWajXGBwmm1jH1ISgxURJ83nSOVQrc_ctBazkZVumKNixsDCaN-GfPU03sfew').then((res)=>{
+      setFlight(res.data);
+    }).catch((err)=>{
+      alert(err);
+    })
+  }
+
   return (
     <div>
       <Main>
@@ -42,24 +64,40 @@ export const Home = () => {
           SEARCH
         </Button>
       </Main>
-      <div>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>Accordion 1</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </div>
+      {flight && flight.map((el)=>(
+
+        <SubMain>
+        <Grid container justifyContent={'space-evenly'}>
+          <Grid item>
+            <Tooltip>
+              <p>{el.airline.name}</p>
+            </Tooltip>
+          </Grid>
+          <Grid item>
+            <Tooltip>
+              <p>{el.arrival.airport}</p>
+            </Tooltip>
+          </Grid>
+          <Grid item sx={{width:'10%',margin:'2%'}}>
+            <Tooltip>
+              <LinearProgress />
+            </Tooltip>
+          </Grid>
+          <Grid item>
+            <Tooltip>
+              <p>{el.departure.airport}</p>
+            </Tooltip>
+          </Grid>
+          <Grid item>
+            <Tooltip>
+              <p>₹ {Math.floor(Math.random() * (12000 -2500) + 2500)}</p>
+            </Tooltip>
+          </Grid>
+        </Grid>
+        </SubMain>
+
+      ))}
+      
     </div>
   );
 };
